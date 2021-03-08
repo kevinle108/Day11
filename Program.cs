ee722753654dab2e5588778e748d0cd720ddecd8
Kevin Le <kevinle108@gmail.com>
2021-03-08 13:03:30 -0500

    Balanced Paren

--- Program.cs
+++ Program.cs
@@ -7,65 +7,41 @@ namespace Day11
     {
         static void Main(string[] args)
         {
-            var ls = new List<string> { "A", "B", "C" };
-            int k = 1;
-            KPermutations(ls, k);
+            int n = 5;
+            Paren(n);
         }
 
-        static void KPermutations(List<string> ls, int k)
+        static void Paren(int n)
         {
-            var results = new List<string>();
-            Console.WriteLine("Using KPermutations1");
-            KPermutations1(ls, results, k);
-            Console.WriteLine("Using Permutations2");
-            KPermutations2(ls, 0, k);
+            Paren(n, n, new List<string>());
         }
 
-        static void KPermutations1(List<string> ls, List<string> results, int k)
+        static void Paren(int open, int close, List<string> results)
         {
-            if (results.Count == k)
-            {
-                PrintList(results);
+            if (open == 0) {
+                List<string> printThis = new List<string>(results);
+                for (int i = 0; i < close; i++)
+                {
+                    printThis.Add(")");
+                }
+                PrintList(printThis);
                 return;
             }
-            if (ls.Count == 0) return;
-            for (int i = 0; i < ls.Count; i++)
+            List<string> rightBranch = new List<string>(results);
+            rightBranch.Add("(");
+            Paren(open - 1, close, rightBranch);
+            if (close > open)
             {
-                var newList = new List<string>(ls);
-                var newResults = new List<string>(results);
-                newList.RemoveAt(i);
-                newResults.Add(ls[i]);
-                KPermutations1(newList, newResults, k);
+                List<string> leftBranch = new List<string>(results);
+                leftBranch.Add(")");
+                Paren(open, close-1, leftBranch);           
             }
-        }
-        static void KPermutations2(List<string> ls, int l, int k)
-        {
-            if (l == k)
-            {
-                PrintList(ls.GetRange(k-1, ls.Count-k-1));
-                return;
-            }
-            for (int i = l; i < ls.Count; i++)
-            {
-                Swap(ls, i, l);
-                KPermutations2(ls, l + 1, k);
-                Swap(ls, i, l);
-            }
-        }
-
-        static void Swap(List<string> ls, int i, int j)
-        {
-            string tmp = ls[i];
-            ls[i] = ls[j];
-            ls[j] = tmp;
         }
 
         static void PrintList(List<string> ls)
         {
-            Console.Write(" { ");
-            foreach (string s in ls)
-                Console.Write(s + " ");
-            Console.WriteLine("}");
+            foreach (string s in ls) Console.Write(s);
+            Console.WriteLine();
         }
     }
 }
